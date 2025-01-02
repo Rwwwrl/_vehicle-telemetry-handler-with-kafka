bc10599d1f3035525637a9ccbc010a3455b4edf6
Alexey Sosov <alexey.sosov@zyfra.com>
2025-01-02 12:06:30 +0400

    feat vehicle_mock_telemetry_generator initial

---
+++ src/apps/vehicle_mock_telemetry_generator/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vehicle_mock_telemetry_generator',
+    version='0.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+)
