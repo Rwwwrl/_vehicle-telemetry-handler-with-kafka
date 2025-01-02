a20db1952921f5c5f09cb99ee6ae49cca7d65298
Alexey Sosov <alexey.sosov@zyfra.com>
2025-01-02 15:18:43 +0400

    feat vehicle_geometry_intersection_ms_events events

---
+++ src/apps/vehicle_geometry_intersection_ms_events/vehicle_geometry_intersection_ms_events/events.py
@@ -0,0 +1,24 @@
+from pydantic.types import PositiveFloat, PositiveInt
+
+from framework.common.dto import DTO
+from framework.integrations_events.integration_event import IntegrationEvent
+
+
+class VehiclePositionLatLon(DTO):
+    lat: PositiveFloat
+    lon: PositiveFloat
+
+
+class VehicleDidMovementEvent(IntegrationEvent):
+    vehicle_id: PositiveInt
+    new_position: VehiclePositionLatLon
+
+
+class VehicleArrivedToLoadingArea(IntegrationEvent):
+    vehicle_id: PositiveInt
+    loading_area_id: PositiveInt
+
+
+class VehicleDeparturedFromLoadingArea(IntegrationEvent):
+    vehicle_id: PositiveInt
+    loading_area_id: PositiveInt
