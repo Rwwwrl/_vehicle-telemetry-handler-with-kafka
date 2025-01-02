54503b5636e50ce8eb2bbecc5ae5460bf9b89d94
Alexey Sosov <alexey.sosov@zyfra.com>
2025-01-02 12:13:05 +0400

    feat vehicle_geometry_intersection_ms_events initial

---
+++ src/apps/vehicle_geometry_intersection_ms_events/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vehicle_geometry_intersection_ms_events',
+    version='0.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+)
